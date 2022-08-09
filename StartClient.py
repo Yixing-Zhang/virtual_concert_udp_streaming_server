92af411b812733ca216610f97ff97fcf83101741
yixing-zhang <u3544946@connect.hku.hk>
2022-08-09 21:43:58 +0800

    Update IP.

--- StartClient.py
+++ StartClient.py
@@ -1,5 +1,5 @@
 import RegistrationClient
 
 if __name__ == "__main__":
-    client = RegistrationClient.RegistrationClient()
+    client = RegistrationClient.RegistrationClient('127.0.0.1', '18.167.110.29', 8192, 1235, 1236, 1237, 14043)
     client.Start()
